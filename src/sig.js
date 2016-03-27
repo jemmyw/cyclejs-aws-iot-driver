c8f6079170e9dfd4f94ad1f7fed8c762ee999021
Jeremy Wells <jemmyw@gmail.com>
2016-03-27 14:16:21 +0200

    Import only used parts of crypto-js

--- src/sig.js
+++ src/sig.js
@@ -1,24 +1,26 @@
 /* eslint semi: 0, max-len: 0, max-params: 0 */
-import CryptoJS from 'crypto-js'
+import HmacSHA256 from 'crypto-js/hmac-sha256'
+import SHA256 from 'crypto-js/sha256'
+import Hex from 'crypto-js/enc-hex'
 import moment from 'moment'
 
 function SigV4Utils() {}
 
 SigV4Utils.sign = function sign(key, msg) {
-  const hash = CryptoJS.HmacSHA256(msg, key);
-  return hash.toString(CryptoJS.enc.Hex);
+  const hash = HmacSHA256(msg, key);
+  return hash.toString(Hex);
 };
 
 SigV4Utils.sha256 = function sha256(msg) {
-  const hash = CryptoJS.SHA256(msg);
-  return hash.toString(CryptoJS.enc.Hex);
+  const hash = SHA256(msg);
+  return hash.toString(Hex);
 };
 
 SigV4Utils.getSignatureKey = function getSignatureKey(key, dateStamp, regionName, serviceName) {
-  const kDate = CryptoJS.HmacSHA256(dateStamp, 'AWS4' + key);
-  const kRegion = CryptoJS.HmacSHA256(regionName, kDate);
-  const kService = CryptoJS.HmacSHA256(serviceName, kRegion);
-  const kSigning = CryptoJS.HmacSHA256('aws4_request', kService);
+  const kDate = HmacSHA256(dateStamp, 'AWS4' + key);
+  const kRegion = HmacSHA256(regionName, kDate);
+  const kService = HmacSHA256(serviceName, kRegion);
+  const kSigning = HmacSHA256('aws4_request', kService);
   return kSigning;
 };
 
